a036537e1ea77d776eab5094060243d32ce5cb6f
Linh Tran <linhtt@ows.vn>
2016-04-18 15:18:00 +0700

    v1 navbar added

--- app/NavBar/NavBar.ts
+++ app/NavBar/NavBar.ts
@@ -2,9 +2,17 @@ import {Component} from 'angular2/core';
 
 @Component({
 	selector:'navbar',
-	templateUrl: 'app/NavBar/NavBar.html'
+	templateUrl: 'app/NavBar/NavBar.html',
+	styleUrls:['app/NavBar/NavBar.css']
 })
 
 export class Navbar {
+	private open = 1;
+	public NavBarTrigger(){
 
+	}
+	constructor()
+	{
+		console.log(this.open);
+	}
 }
