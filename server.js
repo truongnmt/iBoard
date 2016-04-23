cd58d1a7e920b46b13a98a6b6e135199801422a1
Linh Tran <linhtt@ows.vn>
2016-04-24 01:33:17 +0700

    xong vote

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var app =  express();
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
@@ -11,31 +11,47 @@ var path = require('path');
 //  res.sendFile(path.join(__dirname, '', 'index.html'));
 // });
 
+var voteStats = [0, 0, 0, 0, 0];
+
 app.use(express.static('./'));
 
 var userSockets = [""];
 
 // Register events on socket connection
-io.on('connection', function(socket){
-userSockets.push(socket);
-//console.log(socket);
-socket.on('chatMessage', function(from, msg){
-  io.emit('chatMessage', from, msg);
-});
-socket.on('notifyUser', function(user){
-    console.log(user)
-  io.emit('notifyUser', user);
-});
+io.on('connection', function(socket) {
+	userSockets.push(socket);
+	//console.log(socket);
+	socket.on('chatMessage', function(from, msg) {
+		io.emit('chatMessage', from, msg);
+	});
+	socket.on('notifyUser', function(user) {
+		console.log(user)
+		io.emit('notifyUser', user);
+	});
 
-socket.on('caroPlay', function(posX,posY){
-    console.log('caroPlay: ',posX,' ',posY);
-    io.emit('caroPlay',posX,posY);
-})
+	socket.on('caroPlay', function(posX, posY) {
+		console.log('caroPlay: ', posX, ' ', posY);
+		socket.broadcast.emit('caroPlay', posX, posY);
+	})
 
-})
+	socket.on('getData', function() {
+		socket.emit('returnData', voteStats);
+	});
+
+	socket.on('p', function(x) {
+		socket.broadcast.emit('p', x);
+		voteStats[x]++;
+		console.log(voteStats);
+	});
+
+	socket.on('m', function(x) {
+		socket.broadcast.emit('m', x);
+		voteStats[x]--;
+	});
+});
 
 // Listen application request on port 3000
 var port = 8124;
-http.listen(port, function(){
-console.log('listening on *: ', port);
-});
+http.listen(port, function() {
+	console.log('listening on *: ', port);
+})
