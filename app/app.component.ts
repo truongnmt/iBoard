a05db28d6a1939ddf972d431ba6bc0f5a8872e1b
Linh Tran <linhtt@ows.vn>
2016-04-23 11:56:38 +0700

    loi iframe

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,6 @@ import {Component} from 'angular2/core';
 import {Header} from './Header/Header';
 import {MainContent} from './MainContent/MainContent';
 import {Navbar} from './NavBar/NavBar';
-import {Plugins} from './Plugins/Plugins';
 import {Chat} from './Chat/chat';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 
@@ -10,12 +9,12 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/route
 @Component({
     selector: 'my-app',
     templateUrl: './app/app.html',
-    directives: [Header, Navbar, MainContent,Plugins,Chat],
+    directives: [Header, Navbar, MainContent,Chat],
     
 })
 
 @RouteConfig([
-	{path: '/Chat', name: 'Chat', component: Chat},
+	{path: '/chat', name: 'Chat', component: Chat},
 	
 ])
 
